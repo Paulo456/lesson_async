22f7e2c62e249b2b3e2530ac65e8ca37b13f3bf4
Paulo <levchenkopa@it2g.ru>
2022-03-27 23:54:59 +0300

    rocket can do fire

--- main.py
+++ main.py
@@ -2,6 +2,43 @@ import time
 import asyncio
 import curses
 from random import randint, choice
+from curses_tools import draw_frame
+
+with open('rocket_frame_1.txt') as f:
+    rocket_image1 = f.read()
+
+with open('rocket_frame_2.txt') as f:
+    rocket_image2 = f.read()
+
+
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
 
 
 async def blink(canvas, row, column, symbol='*'):
@@ -16,22 +53,38 @@ async def blink(canvas, row, column, symbol='*'):
         await asyncio.sleep(0)
 
 
+async def rocket_fly(canvas, row, column):
+    while True:
+        draw_frame(canvas, row, column, rocket_image2, negative=True)
+        draw_frame(canvas, row, column, rocket_image1)
+        await asyncio.sleep(0)
+
+        draw_frame(canvas, row, column, rocket_image1, negative=True)
+        draw_frame(canvas, row, column, rocket_image2)
+        await asyncio.sleep(0)
+
+
 def draw(canvas):
     curses.curs_set(False)
-    max_window_y, max_window_x = curses.window.getmaxyx(canvas)
-    coroutines = [blink(canvas=canvas, row=randint(0, max_window_y-1), column=randint(0, max_window_x-1), symbol=choice('+*.:'))
+    max_window_y, max_window_x = canvas.getmaxyx()
+    rocket = rocket_fly(canvas, max_window_y - 9, max_window_x/2-2)
+    coroutines = [blink(canvas, randint(0, max_window_y - 1), randint(0, max_window_x - 1), choice('+*.:'))
                   for _ in range(150)]
-    coroutines_copy = coroutines.copy()
+    coroutine_fire = fire(canvas, max_window_y - 1, max_window_x/2)
+    do_fire = True
     while True:
-        try:
-            [coroutine.send(None) for coroutine in coroutines_copy]
-            canvas.refresh()
-            time.sleep(0.5)
-        except StopIteration:
-            coroutines_copy = coroutines.copy()
+        [coroutine.send(None) for coroutine in coroutines]
+        rocket.send(None)
+        if do_fire:
+            try:
+                coroutine_fire.send(None)
+            except StopIteration:
+                do_fire = False
+
+        canvas.refresh()
+        time.sleep(0.2)
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
     curses.wrapper(draw)
-
